db52590fe8132573d227f9e60586b20a9092dc08
yunseuriglitter <yunseuriglitter@gmail.com>
2026-01-31 14:45:37 +0900

    Update game.js

--- game.js
+++ game.js
@@ -1,1 +1,13 @@
+// game.js
 
+let db = [];
+let used = new Set();
+let history = [];
+let lastChar = null;
+
+// 옵션 DOM
+const optPlatformer = document.getElementById("optPlatformer");
+const optNumEdge = document.getElementById("optNumEdge");
+const optNumIgnore = document.getElementById("optNumIgnore");
+const optIgnoreWrap = document.getElementById("optIgnoreWrap");
+const optNoOneShot = document.getElementById("optNoOneShot");
